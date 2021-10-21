b6c53a1b27c603f65e829be706ff98df99951b37
laumart <=>
2021-10-21 20:48:59 -0300

    criacao rota categorias

--- src/main/java/com/lau/petshop/Resources/CategoriaResource.java
+++ src/main/java/com/lau/petshop/Resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.lau.petshop.Resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lau.petshop.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String mostrar() {
-		return "Teste Categoria";
+	public List<Categoria> mostrar() {
+		
+		List<Categoria> lista = new ArrayList();
+		
+		Categoria cat1 = new Categoria(1, "Alimentos");
+		Categoria cat2 = new Categoria(2, "Cosméticos");
+		Categoria cat3 = new Categoria(3, "Remédios");
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		return lista;
 	}
 }
