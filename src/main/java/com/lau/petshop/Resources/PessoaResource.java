c6ab17e230e681f38478ee72388b3366b3bdd0f1
laumart <=>
2021-10-26 17:52:06 -0300

    Disponibilizar endpoint /pessoas/{id}

---
+++ src/main/java/com/lau/petshop/Resources/PessoaResource.java
@@ -0,0 +1,26 @@
+package com.lau.petshop.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lau.petshop.domain.Pessoa;
+import com.lau.petshop.service.PessoaService;
+
+@RestController
+@RequestMapping(value = "/pessoas")
+public class PessoaResource {
+
+	@Autowired
+	PessoaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
+		Pessoa obj = service.find(id);		
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
